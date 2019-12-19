63de6072d752c806ada04392068a0cd359de7693
Isabella Rocha <irocha00@stuy.edu>
2019-12-19 15:35:19 -0500

    End of class

---
+++ sem.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+
+int main() {
+    char * args;
+    fgets(args, 3, stdin);
+    if(strcmp(args, "-c") == 0){
+
+    }
+    if(strcmp(args, "-r") == 0) {
+
+    }
+    if(strcmp(args, "-v") == 0) {
+
+    }
+}
